00aa11d0b9165d548b8284c97d2f195a5ea7ae49
xandeezx10 <166646835+xandeezx10@users.noreply.github.com>
2024-05-09 18:16:57 -0300

    Create questaoQuatro.kt

---
+++ questaoQuatro.kt
@@ -0,0 +1,8 @@
+class retangulo(val base: Double, val altura: Double) {
+    fun calcularArea(): Double {
+        return base * altura
+    }
+    
+    fun calcularPerimetro(): Double {
+        return 2 * (base + altura)
+    }
