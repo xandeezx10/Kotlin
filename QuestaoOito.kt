33fa35ec3529419c1211f33eeaf66d3ac16ece86
xandeezx10 <166646835+xandeezx10@users.noreply.github.com>
2024-05-13 17:51:23 -0300

    Update QuestaoOito.kt

--- QuestaoOito.kt
+++ QuestaoOito.kt
@@ -2,9 +2,8 @@ class Produto(var nome: String, var preco: Double, var quantidade: Int) {
     fun apresentar(): String {
         return "Nome: $nome\nPreço: R$$preco\nQuantidade: $quantidade unidades"
     }
-}
-
-fun main() {
-    val produto = Produto("BOLA DO NEYMAR", 49.99, 10)
-    println(produto.apresentar())
+    
+    fun calcularTotal(): Double {
+        return preco * quantidade
+    }
 }
